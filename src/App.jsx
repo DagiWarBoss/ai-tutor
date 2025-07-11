7319cfebec818b8743d9d78f6e5c4b8012c6b39f
Daksh Malhotra <your_email@example.com>
2025-07-11 19:52:04 +0530

    Sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 // src/App.jsx
 
-// REMOVE or COMMENT OUT THIS LINE:
+// REMOVE OR COMMENT OUT THIS LINE IF IT'S STILL THERE:
 // import TestAI from "./TestAI";
 
 // Import the new ProblemGenerator component.
+// This assumes ProblemGenerator.jsx is directly in the 'src' folder.
 import ProblemGenerator from "./ProblemGenerator";
 
 function App() {
   return (
     <div>
-      {/* REMOVE or COMMENT OUT THIS LINE: */}
+      {/* REMOVE OR COMMENT OUT THIS LINE IF IT'S STILL THERE: */}
       {/* <TestAI /> */}
 
+      {/* Optional: Add a horizontal rule for visual separation */}
       <hr style={{ margin: '50px 0', borderColor: '#eee' }} />
 
+      {/* Render the ProblemGenerator component */}
       <ProblemGenerator />
     </div>
   );
